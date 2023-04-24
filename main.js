c41d9e181f6eb72ddd4bab9591be55de6af61531
Michal Dostal <87271270+MichalDostal@users.noreply.github.com>
2023-04-24 17:10:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 "use strict";
-console.log(document.querySelector(".message").textContent);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
 
-document.querySelector(".message").textContent = "Correct Number👍";
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "🛑 No number!";
+  }
+});
 
 //Michal Dostal 2023
