f5b70b0ead0b105649ce2c893361f8bc285d0ed5
Michal Dostal <87271270+MichalDostal@users.noreply.github.com>
2023-04-25 21:04:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 "use strict";
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "🛑 No number!";
+    document.querySelector(".message").textContent = "⛔️ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Správné číslo";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Číslo je příliš velké";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "Prohrál jsi";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Číslo je příliš malé";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "Prohrál jsi";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
 
