fe2ddc4df42fedcecec972e913272fb29879e0bf
Michal Dostal <87271270+MichalDostal@users.noreply.github.com>
2023-04-26 19:03:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -9,34 +13,30 @@ document.querySelector(".check").addEventListener("click", function () {
 
   // Uživatel nezadal číslo
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️ No number!";
+    displayMessage("Nezadal jste číslo");
 
     // Hráč vyhrál
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Správné číslo";
+    // Dá se také zapsat jako --> document.querySelector(".message").textContent = "Správné číslo"
+    displayMessage("Správné číslo");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
 
-    // Číslo je příliš velké
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Číslo je příliš velké";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "Prohrál jsi";
-      document.querySelector(".score").textContent = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
     }
 
-    //Číslo je příliš malé
-  } else if (guess < secretNumber) {
+    //Pokud je to špatně
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Číslo je příliš malé";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Číslo je příliš velké" : "Číslo je příliš malé";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "Prohrál jsi";
+      displayMessage("Prohrál jsi");
       document.querySelector(".score").textContent = 0;
     }
   }
@@ -45,13 +45,13 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector(".message").textContent = "Začni hádat";
+  displayMessage("Začni hádat");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
 
-  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector("body").style.backgroundColor = "#19376d";
   document.querySelector(".number").style.width = "15rem";
 });
 
-//Michal Dostal 2023
+// Michal Dostal 2023
